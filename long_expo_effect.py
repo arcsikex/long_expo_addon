10a13e83cea071df69196097e715e421360775c0
arcsikex <arcsikex@hotmail.com>
2022-05-11 23:30:21 +0200

    Fix if there is another strip right after. Improve fade in/fade out

--- long_expo_effect.py
+++ long_expo_effect.py
@@ -2,7 +2,7 @@ bl_info = {
     "name": "Long Exposure Video Effect",
     "author": "aaron",
     "description": "Creates a long exposure effect for the selected script",
-    "version": (1, 0),
+    "version": (1, 1),
     "blender": (3, 1, 0),
     "location": "Sequencer -> Strip",
     "warning": "Can use a lot of RAM. Save your scene before using it!",
@@ -60,7 +60,7 @@ class SEQUENCER_OT_long_expo_effect(bpy.types.Operator):
         description="Number of duplicates of the selected strip.",
         default=10,
         min=1,
-        max=128,
+        max=127,
     )
 
     opacity: bpy.props.FloatProperty(
@@ -101,6 +101,7 @@ class SEQUENCER_OT_long_expo_effect(bpy.types.Operator):
             return {"CANCELLED"}
 
         original = bpy.context.scene.sequence_editor.active_strip
+        end_frame = original.frame_final_end
         # Make sure only the active strip is selected:
         bpy.ops.sequencer.select_all(action="DESELECT")
         original.select = True
@@ -127,19 +128,17 @@ class SEQUENCER_OT_long_expo_effect(bpy.types.Operator):
             current.blend_alpha = opacity
 
             if current.channel == 128:
+                self.levels = i
                 # Max number of channels reached
                 break
 
         # Exit from meta strip
         bpy.ops.sequencer.meta_toggle()
 
+        meta_strip = bpy.context.scene.sequence_editor.active_strip
         if not self.fade_in:
-            # Re,pve the first frames as the effect fades in
-            meta_strip = bpy.context.scene.sequence_editor.active_strip
-            bpy.ops.sequencer.select_all(action="DESELECT")
-            meta_strip.select = True
-            meta_strip.select_left_handle = True
-            bpy.ops.transform.seq_slide(value=(self.levels, 0))
+            # Remove the first frames as the effect fades in
+            meta_strip.frame_final_start += self.levels
             # Move the whole strip forward
             bpy.ops.sequencer.select_all(action="DESELECT")
             meta_strip.select = True
@@ -147,11 +146,15 @@ class SEQUENCER_OT_long_expo_effect(bpy.types.Operator):
 
         if self.fade_out:
             # Increase the meta strip lenght
-            meta_strip = bpy.context.scene.sequence_editor.active_strip
-            bpy.ops.sequencer.select_all(action="DESELECT")
-            meta_strip.select = True
-            meta_strip.select_right_handle = True
-            bpy.ops.transform.seq_slide(value=(self.levels, 0))
+            meta_strip.frame_final_end += self.levels
+
+        if end_frame < meta_strip.frame_final_end:
+            """
+            If there is another strip behind it's better to move upone channel.
+            Otherwise it would move on x axis.
+            Not elegant but it works.
+            """
+            meta_strip.channel += 1
 
         return {"FINISHED"}
 
